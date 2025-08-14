f65f8d49b7a1802ddc1e036ac2aaf27d75bdc050
Nicholas Amoo <nickewusi99@gmail.com>
2025-08-14 16:38:39 +0000

    feat: implemented validation for ticket creation

--- tickets/admin.py
+++ tickets/admin.py
@@ -8,3 +8,4 @@ from tickets.models import Ticket
 class TicketAdmin(admin.ModelAdmin):
     list_display = ('sender', 'title', 'created_at', 'assigned_to', 'is_resolved')
     list_filter = ('created_at', 'assigned_to', 'is_resolved')
+    ordering = ('created_at',)
--- tickets/models.py
+++ tickets/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.db import models
 
 # Create your models here.
@@ -12,7 +13,18 @@ class Ticket(models.Model):
     description = models.TextField(blank=False, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     assigned_to = models.OneToOneField(User, on_delete=models.CASCADE, related_name='assigned_tickets')
-    is_resolved = models.BooleanField(default=False) # To determine whether a ticket has been resolved or not
+    is_resolved = models.BooleanField(default=False)  # To determine whether a ticket has been resolved or not
 
     def __str__(self):
         return f"{self.title} by {self.sender}"
+
+    def clean(self):
+        if self.sender.is_agent:
+            raise ValidationError("Agents are not allowed to send Tickets")
+
+        if self.sender == self.assigned_to:
+            raise ValidationError("Ticket cannot be assigned to the same sender")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
