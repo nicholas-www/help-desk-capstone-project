b79acd675d34e291f98f41f2862379365aeef156
Nicholas Amoo <nickewusi99@gmail.com>
2025-08-15 15:22:48 +0000

    feat: add api endpoint for viewing a single ticket

--- tickets/urls.py
+++ tickets/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from tickets import views
 
 urlpatterns = [
-    path('tickets/', views.TicketListAPIView.as_view(), name='tickets')
+    path('tickets/', views.TicketListAPIView.as_view(), name='tickets'),
+    path('tickets/<int:pk>/', views.TicketDetailAPIView.as_view(), name='ticket-detail')
 ]
