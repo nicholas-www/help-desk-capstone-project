6ab208e22b9ba65ee4624368210da241d450a08f
Nicholas Amoo <nickewusi99@gmail.com>
2025-08-15 16:05:38 +0000

    feat: add serializer for creating new tickets

--- tickets/serializers.py
+++ tickets/serializers.py
@@ -7,3 +7,16 @@ class TicketListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
         fields = '__all__'
+
+
+class CreateTicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = ['title', 'description']
+
+    def create(self, validated_data):
+        return Ticket(
+            sender=self.context['request'].user,
+            title=validated_data['title'],
+            description=validated_data['description']
+        )
