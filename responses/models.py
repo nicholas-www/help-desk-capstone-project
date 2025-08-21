8fab267e4a0f628f7f76e11298a60cbba82ed14a
Nicholas Amoo <nickewusi99@gmail.com>
2025-08-21 17:59:40 +0000

    feat: add TicketResponse model

--- responses/models.py
+++ responses/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from tickets.models import Ticket
+
+
+User = get_user_model()
+
+class TicketResponse(models.Model):
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    message = models.TextField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
