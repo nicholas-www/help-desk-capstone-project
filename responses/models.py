af31aaff6e75073f7cd59314bcadf9f5b1834136
Nicholas Amoo <nickewusi99@gmail.com>
2025-08-22 15:05:33 +0000

    feat: add serializer for creating responses

--- responses/models.py
+++ responses/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 from tickets.models import Ticket
 
-
 User = get_user_model()
 
+
 class TicketResponse(models.Model):
-    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE, related_name='responses')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     message = models.TextField(null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
---
+++ responses/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from responses.models import TicketResponse
+
+
+class CreateTicketResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TicketResponse
+        fields = ['title', 'message']
+
