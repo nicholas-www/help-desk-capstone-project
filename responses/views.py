4a6f7982e99912064cb699949e0dd5af88526a2b
Nicholas Amoo <nickewusi99@gmail.com>
2025-08-22 15:11:58 +0000

    feat: add api view for creating responses

--- responses/views.py
+++ responses/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 
-# Create your views here.
+from responses.models import TicketResponse
+from responses.serializers import CreateTicketResponseSerializer
+from tickets.models import Ticket
+from tickets.permissions import IsAgent
+
+
+class CreateTicketResponseAPIView(generics.CreateAPIView):
+    serializer_class = CreateTicketResponseSerializer
+    permission_classes = [IsAgent]  # Only Agents are allowed to respond to tickets
+
+    def perform_create(self, serializer):
+        # Get ticket id from the url i.e api/tickets/<int:id>/respond
+        ticket_id = serializer.kwargs.get('ticket_id')
+        ticket = get_object_or_404(Ticket, id=ticket_id)
+
+        TicketResponse.objects.create(
+            ticket=ticket,
+            sender=self.request.user
+        )
