7fd0e65a6b2b29a3feb9524772a5c35389f9fe76
Nicholas Amoo <nickewusi99@gmail.com>
2025-08-22 15:27:07 +0000

    feat: add TicketResponse to admin

--- responses/admin.py
+++ responses/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from responses.models import TicketResponse
+
+
 # Register your models here.
+
+@admin.register(TicketResponse)
+class ResponseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'ticket', 'sender')
+
